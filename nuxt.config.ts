bbd071062e60a9b634097064742b29a917b420a0
hasthik <hasthikdjathan644@gmail.com>
2023-03-07 02:23:26 +0530

    Adding icon and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+        title: 'Solana Airdrop Faucet',
+          meta: [
+            // <meta name="viewport" content="width=device-width, initial-scale=1">
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+          ],
+          script: [
+           
+          ],
+          link: [
+           
+          ],
+          // please note that this is an area that is likely to change
+          style: [
+           
+          ],
+          noscript: [
+            
+          ]
+        }
+      }
+      
 })
